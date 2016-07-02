5151309c8af8d649d653515a104f9146cdc60830
Daniel Pettifer <danielpettifer@me.com>
2016-07-02 11:32:29 +0100

    added homepage stat base/started modals

--- signup/app/app.ts
+++ signup/app/app.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {Platform, ionicBootstrap} from 'ionic-angular';
+import {Platform, ionicBootstrap,} from 'ionic-angular';
 import {StatusBar} from 'ionic-native';
 import {TabsPage} from './pages/tabs/tabs';
 
